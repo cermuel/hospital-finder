3f7a6ef18684617947b2f0135a05dc1efdbb6344
Ngene Samuel Obasi <81524684+cermuel@users.noreply.github.com>
2024-05-24 20:58:27 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 export default function App() {
+  useEffect(async () => {
+    const test = await axios.get(import.meta.env.VITE_APP_BASE_URL);
+    console.log({ test });
+  }, []);
+
   return (
     <>
       <Router>
